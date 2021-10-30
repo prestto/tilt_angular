6b0776b86002bcb4f9692d2acc2ffc6a97fe56d8
Tom PRESTON <tom.preston377@gmail.com>
2021-10-30 11:55:42 +0100

    added webpack dev server

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,13 +1,27 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-  //...
-  devServer: {
-    // static: {
-    //   directory: path.join(__dirname, 'src'),
-    // },
-    watch: true,
-    compress: true,
-    port: 9000,
+  entry: './src/main.ts',
+  mode: 'development',
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: [/node_modules/],
+      },
+      {
+        test: /\.(js)$/,
+        exclude: [/node_modules/],
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
 };
---
+++ webpack.md
@@ -0,0 +1,9 @@
+# Install
+
+```bash
+# stop angular complaining about the npm version
+npm install -g npm@7.24.2
+
+# install webpack
+npm install --save-dev webpack babel-loader ts-loader webpack-dev-server mini-css-extract-plugin html-webpack-plugin
+```
