839fe55febc975736a674cecbc6c60362c6044d7
Tom PRESTON <tom.preston377@gmail.com>
2021-10-30 12:57:18 +0100

    with error

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.ts',
+  entry: {
+    bundle: './src/main.ts',
+    polyfills: './src/polyfills.ts',
+    // vendor: './src/vendor.ts',
+  },
   mode: 'development',
   resolve: {
     extensions: ['.ts', '.js']
@@ -9,19 +14,33 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        exclude: [/node_modules/],
-      },
-      {
         test: /\.(js)$/,
         exclude: [/node_modules/],
         loader: 'babel-loader',
       },
+      {
+        test: /\.ts$/,
+        use: ['ts-loader', 'angular2-template-loader'],
+        exclude: [/node_modules/],
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    publicPath: '/',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
   },
+  devServer: {
+    historyApiFallback: true,
+  }
 };
--- webpack.md
+++ webpack.md
@@ -5,5 +5,5 @@
 npm install -g npm@7.24.2
 
 # install webpack
-npm install --save-dev webpack babel-loader ts-loader webpack-dev-server mini-css-extract-plugin html-webpack-plugin
+npm install --save-dev webpack babel-loader ts-loader webpack-dev-server mini-css-extract-plugin html-webpack-plugin html-loader angular2-template-loader
 ```
